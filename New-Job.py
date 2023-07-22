fc4fcefcbd83e957587e8030c730294c0f4fbd6c
s0yun <77540086+s0yun@users.noreply.github.com>
2023-07-22 14:29:26 +0100

    Competed basic functionalty

--- New-Job.py
+++ New-Job.py
@@ -1,17 +1,34 @@
 import json
+import datetime
+import os
+import shutil
 
 file_name = "config.json"
 
-# Reading JSON data from the file and loading it into a Python dictionary
-with open(file_name, "r") as file:
-    data = json.load(file)
+try:
+    # Reading JSON config from the file and loading it into a Python dictionary
+    with open(file_name, "r") as file:
+        config = json.load(file)
+except: 
+    print ("An error occured, confirm config is correct or rerun setup")
+    
+# Get the current date and time
+current_date = datetime.datetime.now()
+# Format the current date as "Month Year" string
+current_month_and_year = current_date.strftime("%B %Y")
+jobname = input("What's the job title?")
+companyname = input("Company name?")
 
-print("Loaded data:", data)
 
-# jobname = input("What's the job title?")
-# companyname = input("Company name?")
-
-print(data["JobSearchRoot"])
+# Create  file paths for the application and cover letter for job application
+jobapplicationpath = (f"{config['jobsearchroot']}/{current_month_and_year} - {jobname} ({companyname})")
+coverletter = (f"{jobapplicationpath}/{config['letter']}")
 
 
-jobapplicationpath = (f"")
+# Create directory and then copy the file over and open it. 
+try:
+    os.mkdir(jobapplicationpath)
+    shutil.copy2 (config['draftletterlocation'], coverletter)
+    os.system(r"open '" + coverletter + "'")
+except:
+    print ("An error occured")
